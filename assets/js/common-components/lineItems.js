cc4897e9db47f950f4195446ae8dad27f2f19122
adisakk <adisak_kon@hotmail.com>
2019-06-06 00:37:38 +0700

    SP1 US08 Customer can buy digital product and password checking before open content

--- assets/js/common-components/lineItems.js
+++ assets/js/common-components/lineItems.js
@@ -1,8 +1,51 @@
 var storefrontApp = angular.module('storefrontApp');
 
+storefrontApp.controller('digitalContentPasswordCheckDialogController', ['$scope', '$window', '$uibModalInstance', 'dialogData', '$sce', function ($scope, $window, $uibModalInstance, dialogData, $sce) {
+
+    $scope.dialogData = dialogData;
+
+    $scope.close = function () {
+        console.log("close");
+        $uibModalInstance.close();
+    }
+
+    $scope.checkPassword = function () {
+       
+        if ($scope.dialogData.password == 'Test+001') {
+            $scope.dialogData.digitalContent = "http://www.orimi.com/pdf-test.pdf";
+
+        } else {
+            console.log($scope.pdf.src);
+            $scope.dialogData.passwordCheckError = "Password is not match!";
+        }
+    }
+}]);
+
 storefrontApp.component('vcLineItems', {
     templateUrl: "themes/assets/js/common-components/lineItems.tpl.liquid",
     bindings: {
         items: '='
-    }
+    },
+    require: {
+        accountManager: '^vcAccountManager'
+    },
+    controller:
+        ['dialogService', function (dialogService) {
+
+            var $ctrl = this;
+            $ctrl.type = null;
+            $ctrl.password = "";
+           
+            $ctrl.openDigitalContent = function () {
+                console.log("openDigitalContent");
+                 var dialogData = {
+                    lists: $ctrl.password,
+                    type: $ctrl.type
+                 }
+                dialogService.showDialog(dialogData, 'digitalContentPasswordCheckDialogController', 'storefront.digital-content-password-check-dialog.tpl', function (result) {
+                    console.log(result);
+                });
+            };
+        }
+    ]
 });
